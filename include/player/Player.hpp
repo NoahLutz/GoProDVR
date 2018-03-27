4be4d1d00a6168020b802219d9ceed3e87c33e84
Noah Lutz <noah.l98@gmail.com>
2018-03-27 17:12:49 -0400

    added player class

---
+++ include/player/Player.hpp
@@ -0,0 +1,31 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include<iostream>
+
+#include<vlc/vlc.h>
+#include<gtkmm.h>
+#include<gdk/gdkx.h>
+#include<gtk/gtk.h>
+
+class Player
+{
+    private:
+        libvlc_instance_t *m_vlc_inst;
+        libvlc_media_player_t *m_media_player;
+        libvlc_media_t *m_media;
+    public:
+        Player(libvlc_instance_t *vlc_inst);
+        ~Player();
+        void set_media_player_window(Gtk::Widget *widget); 
+        void set_media(libvlc_media_t *media);
+        void set_media_from_uri(const char *uri);
+        libvlc_media_t* get_media(void);
+        void release_media(void);
+        void pause(void);
+        void play(void);
+        void stop(void);
+        bool is_playing(void);
+};
+
+#endif
---
+++ src/player/Player.cpp
@@ -0,0 +1,60 @@
+#include "player/Player.hpp"
+
+Player::Player(libvlc_instance_t *vlc_inst)
+{
+    m_vlc_inst = vlc_inst;
+    m_media_player = libvlc_media_player_new(m_vlc_inst);
+}
+
+Player::~Player()
+{
+    libvlc_media_player_release(m_media_player);
+}
+
+void Player::set_media_player_window(Gtk::Widget *widget)
+{
+    libvlc_media_player_set_xwindow(m_media_player, GDK_WINDOW_XID(gtk_widget_get_window(widget->gobj())));
+}
+
+void Player::set_media(libvlc_media_t *media)
+{
+    m_media = media;
+    libvlc_media_player_set_media(m_media_player, m_media);
+}
+
+void Player::set_media_from_uri(const char *uri)
+{
+    libvlc_media_t *media = libvlc_media_new_location(m_vlc_inst, uri);
+    this->set_media(media);
+}
+
+libvlc_media_t* Player::get_media(void)
+{
+    return m_media;
+}
+
+void Player::release_media(void)
+{
+    libvlc_media_release(m_media);
+}
+
+void Player::pause(void)
+{
+    libvlc_media_player_pause(m_media_player);
+}
+
+void Player::play(void)
+{
+    libvlc_media_player_play(m_media_player);
+}
+
+void Player::stop(void)
+{
+    this->pause();
+    libvlc_media_player_stop(m_media_player);
+}
+
+bool Player::is_playing(void)
+{
+    return libvlc_media_player_is_playing(m_media_player);
+}
